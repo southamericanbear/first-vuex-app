06acdfb075a63cfb1b0d79b8aa9d28e0cf480599
Alan <dittler.a@gmail.com>
2021-04-15 17:22:30 -0300

    first app made it with vuexJS

---
+++ components/Joke.vue
@@ -0,0 +1,22 @@
+<template>
+  <nuxt-link :to="'jokes/' + id">
+    <div class="joke">
+      <p>{{ joke }}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "Joke",
+  props: ["joke", "id"]
+};
+</script>
+
+<style>
+.joke {
+  padding: 1rem;
+  border: dotted 1px #ccc;
+  margin: 1rem 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><h2>Welcome to the best corny dad jokes in the world</h2></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome To Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
